54e4ce687bedb7dc2e5c62859ec0c88b4a6b95dc
César Pedraza <cpedraza@unal.edu.co>
2023-12-03 12:56:19 -0500

    add naive sum

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,7 +1,17 @@
 use crate::parser::ASTNode;
 
 fn evaluate(node: ASTNode) -> Result<ASTNode, ()> {
-    Ok(node)
+    match node {
+        ASTNode::Sum(lhs, rhs) => sum(lhs, rhs),
+        node => Ok(node),
+    }
+}
+
+fn sum(lhs: Box<ASTNode>, rhs: Box<ASTNode>) -> Result<ASTNode, ()> {
+    match (*lhs, *rhs) {
+        (ASTNode::Integer(l), ASTNode::Integer(r)) => Ok(ASTNode::Integer(l + r)),
+        _ => todo!(),
+    }
 }
 
 #[cfg(test)]
@@ -12,5 +22,18 @@ mod tests {
     #[test]
     fn integer() {
         assert_eq!(evaluate(ASTNode::Integer(1)), Ok(ASTNode::Integer(1)));
+    }
+
+    #[test]
+    fn sum() {
+        assert_eq!(
+            evaluate(
+                ASTNode::Sum(
+                    Box::new(ASTNode::Integer(1)),
+                    Box::new(ASTNode::Integer(1))
+                )
+            ),
+            Ok(ASTNode::Integer(2))
+        );
     }
 }
