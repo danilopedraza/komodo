6766372d49da2248aa43c3c365f0570253c3aff5
Danilo Pedraza <cpedraza@unal.edu.co>
2023-12-24 19:34:04 -0500

    change number trigger in lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -92,7 +92,7 @@ impl Lexer<'_> {
 
     fn integer(&mut self, first: char) -> Token {
         let mut number = String::from(first);
-        while let Some(chr) = self.input.by_ref().next_if(|c| c.is_numeric()) {
+        while let Some(chr) = self.input.by_ref().next_if(|c| matches!(c, '0'..='9')) {
             number.push(chr);
         }
 
