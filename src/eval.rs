7873b248a42dfbd809965df5ca1491a5a7a885f3
Danilo Pedraza <cpedraza@unal.edu.co>
2024-03-03 10:35:15 -0500

    refactor complete

--- src/eval.rs
+++ src/eval.rs
@@ -7,126 +7,12 @@ pub enum EvalError {
     NonExistentOperationError,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum Object {
-    Boolean(bool),
-    Char(char),
-    ExtensionSet(Vec<Object>),
-    Number(i64),
-    String(String),
-    Symbol(String),
-}
-
-pub fn to_string(t: &Object) -> String {
-    match t {
-        Object::Boolean(true) => String::from("true"),
-        Object::Boolean(false) => String::from("false"),
-        Object::ExtensionSet(vec) => vec
-            .iter()
-            .map(to_string)
-            .collect::<Vec<String>>()
-            .join(", "),
-        Object::Number(num) => num.to_string(),
-        Object::Symbol(s) => s.to_string(),
-        Object::Char(chr) => String::from(*chr),
-        Object::String(str) => str.clone(),
-    }
-}
-
-fn remove_repeated(vec: &Vec<ASTNode>) -> Vec<&ASTNode> {
-    let mut res: Vec<&ASTNode> = vec![];
-    for val in vec {
-        if !res.contains(&val) {
-            res.push(val);
-        }
-    }
-
-    res
-}
-
-fn infix(op: InfixOperator, lhs: Object, rhs: Object) -> Result<Object, EvalError> {
-    type O = InfixOperator;
-    type T = Object;
-    Ok(match (op, lhs, rhs) {
-        (O::Equality, T::Symbol(l), T::Symbol(r)) => T::Boolean(l == r),
-        (O::NotEquality, T::Number(l), T::Number(r)) => T::Boolean(l != r),
-        (O::NotEquality, _, _) => T::Boolean(true),
-        (O::Product, T::Number(l), T::Number(r)) => T::Number(l * r),
-        (O::Sum, T::Number(l), T::Number(r)) => T::Number(l + r),
-        (O::Substraction, T::Number(l), T::Number(r)) => T::Number(l - r),
-        (O::Exponentiation, T::Number(l), T::Number(r)) => T::Number(l.pow(r.try_into().unwrap())),
-        (O::Division, T::Number(l), T::Number(r)) => T::Number(l / r),
-        (O::Mod, T::Number(l), T::Number(r)) => T::Number(l % r),
-        (O::GreaterEqual, T::Number(l), T::Number(r)) => T::Boolean(l >= r),
-        (O::Greater, T::Number(l), T::Number(r)) => T::Boolean(l > r),
-        (O::LessEqual, T::Number(l), T::Number(r)) => T::Boolean(l <= r),
-        (O::Less, T::Number(l), T::Number(r)) => T::Boolean(l < r),
-        (O::LogicAnd, T::Boolean(l), T::Boolean(r)) => T::Boolean(l && r),
-        (O::LogicOr, T::Boolean(l), T::Boolean(r)) => T::Boolean(l || r),
-        (O::BitwiseAnd, T::Number(l), T::Number(r)) => T::Number(l & r),
-        (O::BitwiseOr, T::Number(l), T::Number(r)) => T::Number(l | r),
-        (O::BitwiseXor, T::Number(l), T::Number(r)) => T::Number(l ^ r),
-        (O::LeftShift, T::Number(l), T::Number(r)) => T::Number(l << r),
-        (O::RightShift, T::Number(l), T::Number(r)) => T::Number(l >> r),
-        _ => todo!(),
-    })
-}
-
-fn prefix(op: PrefixOperator, expr: Object) -> Result<Object, EvalError> {
-    type P = PrefixOperator;
-    Ok(match (op, expr) {
-        (P::BitwiseNot, Object::Number(num)) => Object::Number(!num),
-        (P::LogicNot, Object::Boolean(val)) => Object::Boolean(!val),
-        (P::Minus, Object::Number(num)) => Object::Number(-num),
-        _ => todo!(),
-    })
-}
-
-fn truthy(val: Object) -> bool {
-    match val {
-        Object::Boolean(res) => res,
-        _ => todo!(),
-    }
-}
 
 fn _truthy(val: _Object) -> bool {
     match val {
         _Object::Boolean(Bool { val }) => val,
         _ => false,
     }
-}
-
-pub fn eval(node: &ASTNode, _env: &Environment) -> Result<Object, EvalError> {
-    Ok(match node {
-        ASTNode::Boolean(val) => Object::Boolean(*val),
-        ASTNode::Integer(str) => Object::Number(str.parse().unwrap()),
-        ASTNode::Symbol(str) => match _env.get(str) {
-            Some(val) => eval(val, _env)?,
-            None => Object::Symbol(str.clone()),
-        },
-        ASTNode::ExtensionSet(lst) => Object::ExtensionSet(
-            remove_repeated(lst)
-                .iter()
-                .map(|val| eval(val, _env).unwrap())
-                .collect(),
-        ),
-        ASTNode::ComprehensionSet(_, _) => todo!(),
-        ASTNode::Let(_, _, val) => eval(val, _env)?,
-        ASTNode::Tuple(_) => todo!(),
-        ASTNode::Signature(_, _) => todo!(),
-        ASTNode::Infix(op, lhs, rhs) => infix(*op, eval(lhs, _env)?, eval(rhs, _env)?)?,
-        ASTNode::Prefix(op, expr) => prefix(*op, eval(expr, _env)?)?,
-        ASTNode::If(cond, true_res, false_res) => {
-            if truthy(eval(cond, _env)?) {
-                eval(true_res, _env)?
-            } else {
-                eval(false_res, _env)?
-            }
-        }
-        ASTNode::Call(_, _) => todo!(),
-        ASTNode::Char(chr) => Object::Char(*chr),
-        ASTNode::String(str) => Object::String(str.clone()),
-    })
 }
 
 pub fn _eval(node: &ASTNode, _env: &Environment) -> Result<_Object, EvalError> {
