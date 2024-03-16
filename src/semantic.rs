66494731faea51e540e98f3e8a14d9eba2dc5845
Danilo Pedraza <cpedraza@unal.edu.co>
2024-03-16 15:45:18 -0500

    add postprocessing for function calls

--- src/semantic.rs
+++ src/semantic.rs
@@ -5,8 +5,19 @@ use crate::ast::{ASTNode, InfixOperator};
 pub fn postprocess(node: ASTNode) -> ASTNode {
     match node {
         ASTNode::Infix(InfixOperator::Correspondence, params, proc) => function(*params, *proc),
+        ASTNode::Infix(InfixOperator::Call, called, args) => call(*called, *args),
         node => node,
     }
+}
+
+fn call(called_node: ASTNode, args_node: ASTNode) -> ASTNode {
+    let called = postprocess(called_node);
+    let args = match postprocess(args_node) {
+        ASTNode::Tuple(v) => v,
+        _ => todo!(),
+    };
+
+    ASTNode::Call(Box::new(called), args)
 }
 
 fn function(params_node: ASTNode, proc_node: ASTNode) -> ASTNode {
@@ -21,4 +32,37 @@ fn function(params_node: ASTNode, proc_node: ASTNode) -> ASTNode {
     };
 
     ASTNode::Function(params, proc)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use crate::ast::{ASTNode, InfixOperator};
+
+    use super::*;
+
+    #[test]
+    fn inlined_function() {
+        let node = ASTNode::Infix(
+            InfixOperator::Call,
+            Box::new(ASTNode::Infix(
+                InfixOperator::Correspondence,
+                Box::new(ASTNode::Symbol(String::from("x"))),
+                Box::new(ASTNode::Symbol(String::from("x"))),
+            )),
+            Box::new(ASTNode::Tuple(vec![ASTNode::Integer(String::from("1"))])),
+        );
+
+        assert_eq!(
+            postprocess(node),
+            ASTNode::Call(
+                Box::new(ASTNode::Function(
+                    vec![String::from("x")],
+                    vec![ASTNode::Symbol(String::from("x"))]
+                )),
+                vec![ASTNode::Integer(String::from("1"))],
+            )
+        );
+    }
 }
