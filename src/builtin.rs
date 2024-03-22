2982348f659b2a1181d332dfcd206e891c795523
Danilo Pedraza <cpedraza@unal.edu.co>
2024-03-22 09:05:05 -0500

    #3 - add getln function

--- src/builtin.rs
+++ src/builtin.rs
@@ -1,12 +1,23 @@
 use crate::{
     env::Environment,
-    object::{Bool, Effect, Object},
+    object::{self, Bool, Effect, Object},
 };
+
+use std::io::{stdin, BufRead};
 
 fn smtc_println(args: &[Object]) -> Object {
     let str = args[0].to_string();
     println!("{str}");
     Object::Boolean(Bool::from(true))
+}
+
+fn smtc_getln(_args: &[Object]) -> Object {
+    let mut line = String::new();
+    stdin().lock().read_line(&mut line).unwrap();
+
+    line.pop();
+
+    Object::String(object::MyString::from(line.as_str()))
 }
 
 pub fn standard_env() -> Environment {
@@ -16,6 +27,13 @@ pub fn standard_env() -> Environment {
         "println",
         crate::object::Object::Function(crate::object::Function::Effect(Effect {
             func: smtc_println,
+        })),
+    );
+
+    env.set(
+        "getln",
+        crate::object::Object::Function(crate::object::Function::Effect(Effect {
+            func: smtc_getln,
         })),
     );
 
