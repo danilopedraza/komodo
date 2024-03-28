287bde11cd2218850ec308dbaae46debb639ec83
Danilo Pedraza <cpedraza@unal.edu.co>
2024-03-28 18:46:06 -0500

    modify test to cover empty list case

--- src/parser.rs
+++ src/parser.rs
@@ -1032,14 +1032,14 @@ mod tests {
 
     #[test]
     fn list() {
-        let input = "[[1], 2]";
+        let input = "[[], 2]";
 
         let lexer = build_lexer(input);
 
         assert_eq!(
             parser_from(lexer.map(|res| res.unwrap())).next(),
             Some(Ok(ASTNode::ExtensionList(vec![
-                ASTNode::ExtensionList(vec![ASTNode::Integer(String::from("1"))]),
+                ASTNode::ExtensionList(vec![]),
                 ASTNode::Integer(String::from("2")),
             ]),)),
         );
