0c9f6880c84df3d01be4909d1716ace7a032cfcc
Danilo Pedraza <cpedraza@unal.edu.co>
2024-04-10 17:50:08 -0500

    #4 - add failing test

--- src/repl.rs
+++ src/repl.rs
@@ -149,4 +149,17 @@ mod tests {
             (String::from("1"), ReplResponse::Continue),
         );
     }
+
+    #[test]
+    #[ignore = "not yet implemented"]
+    fn match_list_prefix() {
+        let mut repl = Repl::default();
+
+        repl.response(Ok(String::from("let f([val|_]) := val")));
+
+        assert_eq!(
+            repl.response(Ok(String::from("f([55])"))),
+            (String::from("55"), ReplResponse::Continue),
+        );
+    }
 }
