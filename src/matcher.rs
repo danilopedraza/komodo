5b58bff12574cf5d5cfbd05f8b524a7d898c8fa3
Danilo Pedraza <cpedraza@unal.edu.co>
2024-04-15 09:48:18 -0500

    extract code into function

--- src/matcher.rs
+++ src/matcher.rs
@@ -14,13 +14,7 @@ pub fn match_and_map(pattern: &ASTNode, val: &Object) -> MatchResult {
     match pattern {
         ASTNode::Wildcard => empty_match(),
         ASTNode::ExtensionList(l) => match_list(l, val),
-        _ => {
-            if exec(pattern, &mut Environment::default()).unwrap() == *val {
-                empty_match()
-            } else {
-                MatchResult::NotAMatch
-            }
-        }
+        _ => match_constant(pattern, val),
     }
 }
 
@@ -35,5 +29,13 @@ fn match_list(pattern: &[ASTNode], val: &Object) -> MatchResult {
             _ => todo!(),
         },
         _ => MatchResult::NotAMatch,
+    }
+}
+
+fn match_constant(pattern: &ASTNode, val: &Object) -> MatchResult {
+    if exec(pattern, &mut Environment::default()).unwrap() == *val {
+        empty_match()
+    } else {
+        MatchResult::NotAMatch
     }
 }
