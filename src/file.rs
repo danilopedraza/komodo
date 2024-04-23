974db12ae42d05910d708f42a90c1a066c1840b1
Danilo Pedraza <cpedraza@unal.edu.co>
2024-04-23 17:23:54 -0500

    make the file parser to return ASTNodes

--- src/file.rs
+++ src/file.rs
@@ -1,13 +1,13 @@
 use std::fs;
 
-use crate::ast::ASTNodeType;
+use crate::ast::ASTNode;
 use crate::lexer::build_lexer;
 use crate::parser::parser_from;
 
-pub fn parse_file(path: &str) -> Vec<ASTNodeType> {
+pub fn parse_file(path: &str) -> Vec<ASTNode> {
     let input = fs::read_to_string(path).unwrap();
     let lexer = build_lexer(&input);
     let mut parser = parser_from(lexer.map(|res| res.unwrap()));
 
-    parser.program()
+    parser.program_()
 }
