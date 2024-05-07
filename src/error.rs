ca1abb19a0c13ed7e607b4295899cb7f213b2bda
Danilo Pedraza <cpedraza@unal.edu.co>
2024-05-07 14:00:04 -0500

    #20 - first error message

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,12 @@
-// use crate::{exec::EvalError, lexer::LexerError, parser::ParserError};
+use crate::{exec::EvalError, lexer::LexerError, parser::ParserError};
 
 // struct Error(ErrorType, Position);
 
-// enum ErrorType {
-//     LexerError(LexerError),
-//     ParserError(ParserError),
-//     ExecError(EvalError),
-// }
+enum Error {
+    LexerError(LexerError),
+    ParserError(ParserError),
+    ExecError(EvalError),
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Position {
@@ -20,8 +20,41 @@ impl Position {
     }
 }
 
-// struct ErrorMessage(Error, Position);
+fn error_msg(err: Error) -> ErrorMessage {
+    match err {
+        Error::ParserError(err) => parser_error_msg(err),
+        _ => todo!(),
+    }
+}
 
-// #[cfg(test)]
-// mod tests {
-// }
+fn parser_error_msg(err: ParserError) -> ErrorMessage {
+    match err {
+        ParserError::ExpectedExpression(_, pos) => {
+            ErrorMessage("Expected an expression, but got a ')'".into(), pos)
+        }
+        _ => todo!(),
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ErrorMessage(String, Position);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lexer::TokenType;
+
+    #[test]
+    fn expected_expression() {
+        assert_eq!(
+            error_msg(Error::ParserError(ParserError::ExpectedExpression(
+                TokenType::Rparen,
+                Position::new(3, 1)
+            ))),
+            ErrorMessage(
+                String::from("Expected an expression, but got a ')'"),
+                Position::new(3, 1),
+            ),
+        );
+    }
+}
