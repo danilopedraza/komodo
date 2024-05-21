68d40c608f0855943c158d724b8bca244717b1e8
Danilo Pedraza <cpedraza@unal.edu.co>
2024-05-21 12:53:25 -0500

    run now returns last object

--- src/run.rs
+++ src/run.rs
@@ -1,13 +1,7 @@
 use std::fs;
 
 use crate::{
-    ast::ASTNode,
-    builtin::standard_env,
-    error::Error,
-    exec::exec,
-    lexer::{build_lexer, Token},
-    parser::{parser_from, Parser},
-    weeder::postprocess,
+    ast::ASTNode, builtin::standard_env, error::Error, exec::exec, lexer::{build_lexer, Token}, object::Object, parser::{parser_from, Parser}, weeder::postprocess
 };
 
 fn collect_nodes<T: Iterator<Item = Result<Token, Error>>>(
@@ -16,16 +10,17 @@ fn collect_nodes<T: Iterator<Item = Result<Token, Error>>>(
     parser.collect()
 }
 
-pub fn run(path: &str) -> Result<(), Error> {
+pub fn run(path: &str) -> Result<Object, Error> {
     let input = fs::read_to_string(path).unwrap();
     let lexer = build_lexer(&input);
     let parser = parser_from(lexer);
     let nodes = collect_nodes(parser)?;
 
     let mut env = standard_env();
+    let mut obj = Object::empty_tuple();
     for node in nodes {
-        exec(&postprocess(node), &mut env)?;
+        obj = exec(&postprocess(node), &mut env)?;
     }
 
-    Ok(())
+    Ok(obj)
 }
