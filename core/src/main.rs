8ae9f5ce9e97ded70ef1abbc1605862132cda1d3
Danilo Pedraza <cpedraza@unal.edu.co>
2024-06-12 13:36:55 -0500

    interpreter exits with 1 if an error appears, with 0 otherwise

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,19 +1,27 @@
 use std::fs;
+use std::process::ExitCode;
 
 use symstatic::error::error_msg;
 use symstatic::repl::{repl, MyCLI};
 use symstatic::run::run;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
+fn run_smtc(args: &[String]) -> ExitCode {
     if args.len() == 1 {
         repl(&mut MyCLI::default());
+        ExitCode::SUCCESS
     } else {
         let input = fs::read_to_string(&args[1]).unwrap();
         let res = run(&input);
         if let Err(err) = res {
             error_msg(&err).emit(&args[1], &input);
+            ExitCode::FAILURE
+        } else {
+            ExitCode::SUCCESS
         }
     }
+}
+
+fn main() -> ExitCode {
+    let args: Vec<String> = std::env::args().collect();
+    run_smtc(&args)
 }
