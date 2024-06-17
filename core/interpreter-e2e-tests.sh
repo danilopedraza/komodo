1a206d7fa0451312772ce827fe38169643e6c4ac
Danilo Pedraza <cpedraza@unal.edu.co>
2024-06-17 18:54:35 -0500

    pass all e2e error messages to stderr and put cargo run quiet

--- core/interpreter-e2e-tests.sh
+++ core/interpreter-e2e-tests.sh
@@ -3,11 +3,11 @@
 examples_dir="$(pwd)/../examples"
 
 for file in $(find "$examples_dir" -type f -name "*.smtc"); do
-    output=$(eval "cargo run $file" 2>&1)
+    output=$(eval "cargo run --quiet $file" 2>&1)
 
     if [ $? -ne 0 ]; then
-        echo "Error: $file failed its execution"
-        echo "This is the message from the interpreter:\n"
+        echo "Error: $file failed its execution" >&2
+        echo "This is the message from the interpreter:\n" >&2
         echo "$output" >&2
         exit 1
     fi
