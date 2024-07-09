7efbaaf9e43c451f48df9142026a43a891e35d3b
Danilo Pedraza <cpedraza@unal.edu.co>
2024-07-09 16:20:51 -0500

    add naive impl of wasm interface

--- core-browser/src/lib.rs
+++ core-browser/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use symstatic::{env::Environment, error::error_msg, run::run};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,6 +9,25 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, core-browser!");
+pub fn run_code(source: &str) -> Option<String> {
+    if let Err(err) = run(source, &mut Environment::default()) {
+        Some(String::from_utf8(error_msg(&err).as_bytes("foo", source)).unwrap())
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::run_code;
+
+    #[test]
+    fn correct_code() {
+        assert!(run_code("2 + 2").is_none());
+    }
+
+    #[test]
+    fn incorrect_code() {
+        assert!(run_code("3 + (").is_some());
+    }
 }
