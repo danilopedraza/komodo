c148e92eca69cca5e588107022365d992dbc786b
Danilo Pedraza <cpedraza@unal.edu.co>
2024-07-10 06:17:33 -0500

    add build step to tests and put all of rust infra into devenv, as well as wasm-pack

--- core-browser/test.sh
+++ core-browser/test.sh
@@ -7,3 +7,5 @@ cargo fmt --check
 cargo clippy --all-targets --all-features
 
 cargo test --all-features
+
+wasm-pack build --target web
--- devenv.nix
+++ devenv.nix
@@ -2,10 +2,11 @@
 
 {
   packages = [
-    pkgs.cargo
     pkgs.git
     pkgs.mdbook
     pkgs.nodejs_20
+    pkgs.rustup
+    pkgs.wasm-pack
   ];
 
   # See full reference at https://devenv.sh/reference/options/
