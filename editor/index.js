b055c19162a98ce2cfd2e0bdedf89ba7006a19db
Danilo Pedraza <cpedraza@unal.edu.co>
2024-07-11 14:37:40 -0500

    put sample text in results section

--- editor/index.js
+++ editor/index.js
@@ -4,7 +4,6 @@ editor.session.setMode("ace/mode/javascript");
 
 let results = ace.edit("results");
 results.setTheme("ace/theme/dracula");
-results.session.setMode("ace/mode/javascript");
 results.setReadOnly(true);
 results.setShowPrintMargin(false);
 results.renderer.setShowGutter(false);
