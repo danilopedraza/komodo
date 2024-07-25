6de45524176d27c844423565f3f5ce60af98f848
Danilo Pedraza <cpedraza@unal.edu.co>
2024-07-25 18:18:30 -0500

    add emojis to e2e interpreter tests messages and fix error

--- core/test.sh
+++ core/test.sh
@@ -8,17 +8,18 @@ cargo clippy --all-targets --all-features
 
 cargo test --all-features
 
+set +e
 examples_dir="$(pwd)/../examples"
 echo "Running examples..."
 for file in $(find "$examples_dir" -type f -name "*.smtc"); do
     output=$(eval "cargo run --quiet $file" 2>&1)
 
     if [ $? -ne 0 ]; then
-        echo "Error: $file failed its execution" >&2
-        echo "This is the message from the interpreter:\n" >&2
+        echo "❌ Error: $file failed its execution" >&2
+        echo "This is the message from the interpreter:" >&2
         echo "$output" >&2
         exit 1
     fi
 done
 
-echo "All of the example files were executed correctly!"
+echo "✅ All of the example files were executed correctly!"
