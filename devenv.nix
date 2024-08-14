dd617d53a4baef820b6f180c04357a2940536f17
Danilo Pedraza <cpedraza@unal.edu.co>
2024-08-14 10:55:39 -0500

    stop using rustp nixpkg, replace with rust pkg from fenix

--- devenv.nix
+++ devenv.nix
@@ -5,14 +5,24 @@
     pkgs.git
     pkgs.mdbook
     pkgs.nodejs_20
-    pkgs.rustup
+    pkgs.wasm-bindgen-cli
     pkgs.wasm-pack
     pkgs.vsce
   ];
 
+  languages = {
+    rust = {
+      enable = true;
+      channel = "stable";
+      targets = [
+        "x86_64-unknown-linux-gnu"
+        "wasm32-unknown-unknown"
+      ];
+    };
+  };
+
   enterShell = ''
     source autocomplete.sh
-    rustup default stable
   '';
 
   # See full reference at https://devenv.sh/reference/options/
