db69264f7b118904d022fa80e2de1e5825f99b8f
Danilo Pedraza <cpedraza@unal.edu.co>
2024-12-03 13:19:20 -0500

    #54 - add messages and fail at first error in installer

--- installer/linux/install.sh
+++ installer/linux/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+set -e
+echo "Installing Komodo... (this will require root access)"
+
+cd /tmp
 wget https://github.com/danilopedraza/komodo/releases/download/v0.1.0/komodo
-chmod +x komodo
-mv komodo /usr/local/bin
+sudo chmod +x komodo
+sudo mv komodo /usr/local/bin/komodo
+
+echo "Komodo is now installed! Try the REPL by typing 'komodo'"
