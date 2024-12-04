83cb06b81726225a56ba3878400f1d2856084da4
Danilo Pedraza <cpedraza@unal.edu.co>
2024-12-04 18:13:13 -0500

    put both commands as root in install script

--- installer/linux/install.sh
+++ installer/linux/install.sh
@@ -5,6 +5,7 @@ echo "Installing Komodo... (this will require root access)"
 
 cd /tmp
 wget https://github.com/danilopedraza/komodo/releases/download/v0.1.0/komodo
-sudo chmod +x komodo && mv komodo /usr/local/bin/komodo
+sudo chmod +x komodo
+sudo mv komodo /usr/local/bin/komodo
 
 echo "Komodo is now installed! Try the REPL by typing 'komodo'"
