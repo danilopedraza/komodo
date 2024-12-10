5835cdcc76cf94c4264b504618288f52a4b42c65
Danilo Pedraza <cpedraza@unal.edu.co>
2024-12-10 15:16:27 -0500

    add test for stdlib installation and modify test message

--- installer/linux/test.sh
+++ installer/linux/test.sh
@@ -6,7 +6,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://komodo-lang.org/install.sh | sh
 
 if [ -f /usr/local/bin/komodo ]
 then
-    echo "✅ the binary has been moved successfully."
+    echo "✅ the binary has been moved to /usr/local/bin/ successfully."
 else
     echo "❌ the binary was not moved to /usr/bin/local."
     exit 1
@@ -19,3 +19,17 @@ else
     echo "❌ the binary is not executable."
     exit 1
 fi
+
+declare -a modules=("utils")
+
+for module in "${modules[@]}"
+do
+    if [ ! -f "/usr/local/lib/komodo/$module.komodo" ]
+    then
+        echo "❌ the module '$module.komodo' is not in /usr/local/lib/komodo/."
+        echo "The standard library is incomplete."
+        exit 1
+    fi
+done
+
+echo "✅ The standard library has been installed successfully."
