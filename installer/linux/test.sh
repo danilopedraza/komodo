984e39080f80c6b8ca481511e125780a91e6a19e
Danilo Pedraza <cpedraza@unal.edu.co>
2024-12-10 11:48:21 -0500

    add messages to installer test script

--- installer/linux/test.sh
+++ installer/linux/test.sh
@@ -3,4 +3,19 @@
 set -e
 
 curl --proto '=https' --tlsv1.2 -sSf https://komodo-lang.org/install.sh | sh
-test -f /usr/local/bin/komodo
+
+if [ -f /usr/local/bin/komodo ]
+then
+    echo "✅ the binary has been moved successfully."
+else
+    echo "❌ the binary was not moved to /usr/bin/local."
+    exit 1
+fi
+
+if [ -x /usr/local/bin/komodo ]
+then
+    echo "✅ the binary is executable."
+else
+    echo "❌ the binary is not executable."
+    exit 1
+fi
