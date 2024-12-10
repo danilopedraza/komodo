0e2496d110785c31152c670f9459479d4489c404
Danilo Pedraza <cpedraza@unal.edu.co>
2024-12-10 17:51:03 -0500

    update install script

--- installer/linux/install.sh
+++ installer/linux/install.sh
@@ -8,4 +8,13 @@ wget https://github.com/danilopedraza/komodo/releases/download/v0.2.0/komodo
 sudo chmod +x komodo
 sudo mv komodo /usr/local/bin/komodo
 
+echo "Installing the standard library..."
+sudo mkdir -p /usr/local/lib/komodo
+
+for module in utils
+do
+    wget https://github.com/danilopedraza/komodo/releases/download/v0.2.0/$module.komodo
+    sudo mv $module.komodo /usr/local/lib/komodo/$module.komodo
+done
+
 echo "Komodo is now installed! Try the REPL by typing 'komodo'"
