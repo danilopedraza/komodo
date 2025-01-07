c8398914d74148cf5a9c5727914c8bc1af9eb0fe
Danilo Pedraza <cpedraza@unal.edu.co>
2025-01-07 18:06:34 -0500

    remove single quotes in char format

--- core/src/object/mod.rs
+++ core/src/object/mod.rs
@@ -445,7 +445,7 @@ impl PrefixOperable for Char {}
 
 impl fmt::Display for Char {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "'{}'", self.val)
+        write!(f, "{}", self.val)
     }
 }
 
