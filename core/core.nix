7abaa280a583c65b6867587821763abc5ca880b9
César Pedraza <cpedraza@unal.edu.co>
2025-03-26 20:31:50 -0500

    #96 - add interpreter nix build

---
+++ core/core.nix
@@ -0,0 +1,20 @@
+{
+  buildCargoPackage,
+  diffutils,
+  gnum4,
+  gnumake,
+  libgcc,
+}:
+
+buildCargoPackage {
+  src = ./.;
+  
+  nativeBuildInputs = [
+    # These dependencies are necessary to build Rug,
+    # which (indirectly) recompiles GMP and MPFR...
+    diffutils
+    gnum4
+    gnumake
+    libgcc
+  ];
+}
--- default.nix
+++ default.nix
@@ -1,9 +1,15 @@
 let
   nixpkgs = import ./nixpkgs.nix;
   pkgs = import nixpkgs { };
+  naersk = pkgs.callPackage (fetchTarball "https://github.com/nix-community/naersk/archive/refs/heads/master.zip") { };
 in
 {
   book = pkgs.callPackage ./book/book.nix { };
+
+  core = pkgs.callPackage ./core/core.nix {
+    buildCargoPackage = naersk.buildPackage;
+  };
+
   report = pkgs.callPackage ./report/report.nix {
     buildTypstDocument = pkgs.callPackage ./report/build-typst-document.nix { };
   };
