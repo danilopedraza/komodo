8abffc9ae7079c210efac14035f88f23011c8369
César Pedraza <cpedraza@unal.edu.co>
2025-03-26 21:02:40 -0500

    #96 - replace tarball import with hashed git import

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
 let
   nixpkgs = import ./nixpkgs.nix;
   pkgs = import nixpkgs { };
-  naersk = pkgs.callPackage (fetchTarball "https://github.com/nix-community/naersk/archive/refs/heads/master.zip") { };
+  naersk = pkgs.callPackage (builtins.fetchGit {
+    url = "https://github.com/nix-community/naersk";
+    rev = "a75c0584b0d69de943babc899530e9c70c642b42";
+  }) {};
 in
 {
   book = pkgs.callPackage ./book/book.nix { };
