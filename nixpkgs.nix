bc50d6951fa898d76dc9c5be5f7be6c126e9acac
César Pedraza <cpedraza@unal.edu.co>
2025-03-31 18:10:46 -0500

    #96 - hash nixpkgs so it can be cached

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,7 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+    sha256 = "08pwvljh8qskw8ajggfpx4x2jc1x69jc9vqkqzx68aj66vb2rsmh";
+  };
 in
   nixpkgs
