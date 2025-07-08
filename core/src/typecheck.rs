cbd9105fd22689fdc9eab9f290723e9b040c8a39
César Pedraza <cpedraza@unal.edu.co>
2025-07-07 19:53:11 -0500

    #35 - start doing yin yang type shit with the type checker

--- core/src/typecheck.rs
+++ core/src/typecheck.rs
@@ -4,6 +4,7 @@ use crate::ast::{ASTNode, ASTNodeKind};
 enum SingleType {
     Float,
     Integer,
+    Unknown,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -13,26 +14,42 @@ enum Type {
 }
 
 fn check(val: &ASTNode, sig: Type) -> bool {
-    match sig {
-        Type::Single(single_type) => check_single_type(val, single_type),
-        Type::Either(lsig, rsig) => check_either_type(val, lsig, *rsig),
-    }
-}
-
-fn check_single_type(val: &ASTNode, sig: SingleType) -> bool {
-    match (&val.kind, sig) {
-        (ASTNodeKind::Integer { .. }, SingleType::Integer) => true,
-        (ASTNodeKind::Decimal { .. }, SingleType::Float) => true,
-        _ => false,
-    }
-}
-
-fn check_either_type(val: &ASTNode, lsig: SingleType, rsig: Type) -> bool {
-    false
+    infer(val) == sig
 }
 
 fn infer(val: &ASTNode) -> Type {
-    Type::Single(SingleType::Integer)
+    match &val.kind {
+        ASTNodeKind::Integer { .. } => Type::Single(SingleType::Integer),
+        ASTNodeKind::Decimal { .. } => Type::Single(SingleType::Float),
+        ASTNodeKind::AdInfinitum => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Assignment { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Boolean(_) => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Block(_) => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Call { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Case { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Char(_) => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Comprehension { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::DotNotation { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::IndexNotation { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Dictionary { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::List { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Set { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::For { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Function { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Fraction { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::If { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::ImportFrom { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Infix { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Declaration(_) => Type::Single(SingleType::Unknown),
+        ASTNodeKind::TaggedExpression { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Prefix { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Cons { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::SetCons { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::String { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Symbol { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Tuple { .. } => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Wildcard => Type::Single(SingleType::Unknown),
+    }
 }
 
 #[cfg(test)]
@@ -65,6 +82,14 @@ mod tests {
             &decimal("0", "0", dummy_pos()),
             Type::Single(SingleType::Float),
         ))
+    }
+
+    #[test]
+    fn float_infer() {
+        assert_eq!(
+            infer(&decimal("0", "0", dummy_pos())),
+            Type::Single(SingleType::Float)
+        );
     }
 
     #[test]
