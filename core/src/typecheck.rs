4746ebf61268c8b8959dd04e906f67237d4de224
César Pedraza <cpedraza@unal.edu.co>
2025-07-07 19:35:45 -0500

    #35 - change function signatures

--- core/src/typecheck.rs
+++ core/src/typecheck.rs
@@ -12,26 +12,26 @@ enum Type {
     Either(SingleType, Box<Type>),
 }
 
-fn check(val: ASTNode, sig: Type) -> bool {
+fn check(val: &ASTNode, sig: Type) -> bool {
     match sig {
         Type::Single(single_type) => check_single_type(val, single_type),
         Type::Either(lsig, rsig) => check_either_type(val, lsig, *rsig),
     }
 }
 
-fn check_single_type(val: ASTNode, sig: SingleType) -> bool {
-    match (val.kind, sig) {
+fn check_single_type(val: &ASTNode, sig: SingleType) -> bool {
+    match (&val.kind, sig) {
         (ASTNodeKind::Integer { .. }, SingleType::Integer) => true,
         (ASTNodeKind::Decimal { .. }, SingleType::Float) => true,
         _ => false,
     }
 }
 
-fn check_either_type(val: ASTNode, lsig: SingleType, rsig: Type) -> bool {
+fn check_either_type(val: &ASTNode, lsig: SingleType, rsig: Type) -> bool {
     false
 }
 
-fn infer(val: ASTNode) -> Type {
+fn infer(val: &ASTNode) -> Type {
     Type::Single(SingleType::Integer)
 }
 
@@ -46,32 +46,32 @@ mod tests {
     #[test]
     fn integer_check() {
         assert!(check(
-            dec_integer("10", dummy_pos()),
-            Type::Single(SingleType::Integer)
+            &dec_integer("10", dummy_pos()),
+            Type::Single(SingleType::Integer),
         ))
     }
 
     #[test]
     fn integer_infer() {
         assert_eq!(
-            infer(dec_integer("0", dummy_pos())),
-            Type::Single(SingleType::Integer)
+            infer(&dec_integer("0", dummy_pos())),
+            Type::Single(SingleType::Integer),
         );
     }
 
     #[test]
     fn float_check() {
         assert!(check(
-            decimal("0", "0", dummy_pos()),
-            Type::Single(SingleType::Float)
+            &decimal("0", "0", dummy_pos()),
+            Type::Single(SingleType::Float),
         ))
     }
 
     #[test]
     fn bad_check() {
         assert!(!check(
-            decimal("0", "0", dummy_pos()),
-            Type::Single(SingleType::Integer)
+            &decimal("0", "0", dummy_pos()),
+            Type::Single(SingleType::Integer),
         ))
     }
 }
