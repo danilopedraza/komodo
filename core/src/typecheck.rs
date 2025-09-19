565dd158cf99d02fdcd07d1fb34cf316ffb87149
César Pedraza <cpedraza@unal.edu.co>
2025-09-18 19:10:59 -0500

    #35 - add dead code exceptions to all of this, turn signature into result

--- core/src/typecheck.rs
+++ core/src/typecheck.rs
@@ -1,5 +1,13 @@
-use crate::ast::{ASTNode, ASTNodeKind};
+use crate::{
+    ast::{ASTNode, ASTNodeKind},
+    error::Position,
+};
 
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Eq)]
+enum TypeError {}
+
+#[allow(dead_code)]
 #[derive(Debug, PartialEq, Eq)]
 enum SingleType {
     Float,
@@ -7,48 +15,51 @@ enum SingleType {
     Unknown,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, PartialEq, Eq)]
 enum Type {
     Single(SingleType),
     Either(SingleType, Box<Type>),
 }
 
-fn check(val: &ASTNode, sig: Type) -> bool {
-    infer(val) == sig
+#[allow(dead_code)]
+fn check(val: &ASTNode, sig: Type) -> Result<bool, (TypeError, Position)> {
+    Ok(infer(val)? == sig)
 }
 
-fn infer(val: &ASTNode) -> Type {
+#[allow(dead_code)]
+fn infer(val: &ASTNode) -> Result<Type, (TypeError, Position)> {
     match &val.kind {
-        ASTNodeKind::Integer { .. } => Type::Single(SingleType::Integer),
-        ASTNodeKind::Decimal { .. } => Type::Single(SingleType::Float),
-        ASTNodeKind::AdInfinitum => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Assignment { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Boolean(_) => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Block(_) => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Call { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Case { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Char(_) => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Comprehension { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::DotNotation { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::IndexNotation { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Dictionary { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::List { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Set { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::For { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Function { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Fraction { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::If { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::ImportFrom { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Infix { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Declaration(_) => Type::Single(SingleType::Unknown),
-        ASTNodeKind::TaggedExpression { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Prefix { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Cons { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::SetCons { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::String { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Symbol { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Tuple { .. } => Type::Single(SingleType::Unknown),
-        ASTNodeKind::Wildcard => Type::Single(SingleType::Unknown),
+        ASTNodeKind::Integer { .. } => Ok(Type::Single(SingleType::Integer)),
+        ASTNodeKind::Decimal { .. } => Ok(Type::Single(SingleType::Float)),
+        ASTNodeKind::AdInfinitum => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Assignment { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Boolean(_) => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Block(_) => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Call { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Case { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Char(_) => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Comprehension { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::DotNotation { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::IndexNotation { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Dictionary { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::List { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Set { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::For { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Function { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Fraction { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::If { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::ImportFrom { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Infix { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Declaration(_) => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::TaggedExpression { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Prefix { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Cons { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::SetCons { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::String { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Symbol { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Tuple { .. } => Ok(Type::Single(SingleType::Unknown)),
+        ASTNodeKind::Wildcard => Ok(Type::Single(SingleType::Unknown)),
     }
 }
 
@@ -62,41 +73,50 @@ mod tests {
 
     #[test]
     fn integer_check() {
-        assert!(check(
-            &dec_integer("10", dummy_pos()),
-            Type::Single(SingleType::Integer),
-        ))
+        assert_eq!(
+            check(
+                &dec_integer("10", dummy_pos()),
+                Type::Single(SingleType::Integer),
+            ),
+            Ok(true)
+        )
     }
 
     #[test]
     fn integer_infer() {
         assert_eq!(
             infer(&dec_integer("0", dummy_pos())),
-            Type::Single(SingleType::Integer),
+            Ok(Type::Single(SingleType::Integer)),
         );
     }
 
     #[test]
     fn float_check() {
-        assert!(check(
-            &decimal("0", "0", dummy_pos()),
-            Type::Single(SingleType::Float),
-        ))
+        assert_eq!(
+            check(
+                &decimal("0", "0", dummy_pos()),
+                Type::Single(SingleType::Float),
+            ),
+            Ok(true)
+        )
     }
 
     #[test]
     fn float_infer() {
         assert_eq!(
             infer(&decimal("0", "0", dummy_pos())),
-            Type::Single(SingleType::Float)
+            Ok(Type::Single(SingleType::Float))
         );
     }
 
     #[test]
     fn bad_check() {
-        assert!(!check(
-            &decimal("0", "0", dummy_pos()),
-            Type::Single(SingleType::Integer),
-        ))
+        assert_eq!(
+            check(
+                &decimal("0", "0", dummy_pos()),
+                Type::Single(SingleType::Integer),
+            ),
+            Ok(true)
+        )
     }
 }
