eb66be8a6ec899cf83ac33759b077802eef24dc3
César Pedraza <cpedraza@unal.edu.co>
2026-01-07 08:41:49 -0500

    update to nixpkgs 25.11

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz";
-    sha256 = "1gx0hihb7kcddv5h0k7dysp2xhf1ny0aalxhjbpj2lmvj7h9g80a";
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.11.tar.gz";
+    sha256 = "1zn1lsafn62sz6azx6j735fh4vwwghj8cc9x91g5sx2nrg23ap9k";
   };
 in
   nixpkgs
